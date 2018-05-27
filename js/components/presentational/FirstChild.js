77d1a336bd14fd9c1aa8843a19ba5839473a5035
Paul Pepper <pm_pepper@hotmail.com>
2018-05-27 07:53:53 +0100

    Remove unused presentational components

--- js/components/presentational/FirstChild.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-//This component is Pure
-
-export default function FirstChild({children}) {
-  const childrenArray = React.Children.toArray(children);
-  return childrenArray[0] || null;
-};
